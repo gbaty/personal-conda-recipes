64eb0eeb0e5a0db20aa33323d407df4d2b24b5f9
Guillaume Baty <guillaume.baty@inria.fr>
2015-09-07 18:10:17 +0200

    Doxygen recipe, first draft

---
+++ doxygen/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+
+mkdir build
+cd build
+
+source $PREFIX/etc/conda/activate.d/*.sh
+
+$PREFIX/bin/cmake \
+    -DCMAKE_INSTALL_PREFIX:PATH="$PREFIX" \
+    -DCMAKE_INSTALL_RPATH:STRING="$PREFIX" \
+    -DBUILD_SHARED_LIBS:BOOL=ON \
+    -Dbuild_app:BOOL=ON \
+    -Dbuild_wizard:BOOL=ON \
+    ..
+
+
+    
+    
+make -j${CPU_COUNT}
+make install
+
+cp -v lib/* $PREFIX/lib/ # Else do not work with BUILD_SHARD_LIBS
+# cp -rv include/* $PREFIX/include/
+# cp -rv doc/* $PREFIX/doc/
+
+source $PREFIX/etc/conda/deactivate.d/*.sh
